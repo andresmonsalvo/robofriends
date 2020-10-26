e9062744db01a745f9164c67a3895b6bc1c50625
Andres Monsalvo <69931247+andresmonsalvo@users.noreply.github.com>
2020-10-26 17:44:16 -0600

    general updates

--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import Hello from "./Hello";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "tachyons";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Hello greeting={"Hello " + "React Ninja"} />
     <App />
   </React.StrictMode>,
   document.getElementById("root")
